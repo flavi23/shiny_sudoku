76e19bcc0a77f2da6d714ec337d57ab188e45113
flavi23 <flaviebenard@hotmail.fr>
2022-02-11 09:52:07 +0100

    Added start of matrix_grid function for generating sudoku grids

--- shinysud/R/matrix_grid.R
+++ shinysud/R/matrix_grid.R
@@ -1,8 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
@@ -13,6 +8,9 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
+matrix_grid <- function()
+  {
+  Matrix1 <- matrix(data = sample(1:9, replace = F), nrow = 1, ncol = 9)
+  Matrix2 <- rbind(Matrix1,c(Matrix1[7:9],Matrix1[1:3],Matrix1[4:6]))
+  Matrix3 <- rbind(Matrix2,c(Matrix2[2,7:9],Matrix2[2,1:3],Matrix2[2,4:6]))
+  }
